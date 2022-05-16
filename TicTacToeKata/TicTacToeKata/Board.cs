eb25199b86e3ba2a607099f928329cb76948fef3
evaderrama <evalderrama@domingoalonsogroup.com>
2022-05-16 23:39:35 +0100

    ♻

--- TicTacToeKata/TicTacToeKata/Board.cs
+++ TicTacToeKata/TicTacToeKata/Board.cs
@@ -7,17 +7,28 @@ public class Board
 
     public static void Main(){}
         
-    public void Play(Player player, int X, int Y)
+    public void Play(Player player, int x, int y)
+    {
+        CheckPlayerTurn(player);
+
+        CheckForValidPosition(x, y);
+
+        _boardCoordinates[x, y] = player.ToString();
+
+    }
+
+    private void CheckForValidPosition(int X, int Y)
+    {
+        if (!string.IsNullOrEmpty(_boardCoordinates[X, Y]))
+            throw new IncorrectPosition("Can't play in already used position !!!");
+    }
+
+    private void CheckPlayerTurn(Player player)
     {
         if (player == Player.O && _lastPlayer == Player.Null) throw new IncorrectTurnException("X player must be first");
 
-        if (player.Equals(_lastPlayer)) throw  new IncorrectTurnException("Incorrect Turn !!, player can not play twice.");
-            
+        if (player.Equals(_lastPlayer)) throw new IncorrectTurnException("Incorrect Turn !!, player can not play twice.");
+
         _lastPlayer = player;
-
-        if (!string.IsNullOrEmpty(_boardCoordinates[X, Y])) throw new IncorrectPosition("Can't play in already used position !!!");
-
-        _boardCoordinates[X, Y] = player.ToString();
-
     }
 }
