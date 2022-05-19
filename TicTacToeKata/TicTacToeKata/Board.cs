91a4782684864baa834c27e6710dd3b27a0fda8e
evaderrama <evalderrama@domingoalonsogroup.com>
2022-05-19 22:16:52 +0100

    :recycle: Refactor Code

--- TicTacToeKata/TicTacToeKata/Board.cs
+++ TicTacToeKata/TicTacToeKata/Board.cs
@@ -8,14 +8,31 @@ public class Board
     private Player? _lastPlayer;
     private Player? _winner;
     private readonly List<Player> _winningPlayers = new() { Player.X, Player.O };
+    private readonly List<WinningCellSet> _winningCellSetList;
+
     private readonly Dictionary<BoardCells,Player> _boardCells= new ();
+    private readonly WinningCellSet _winningTopCellSet = new(new List<BoardCells> { BoardCells.TopLeft, BoardCells.TopMiddle, BoardCells.TopRigth });
+    private readonly WinningCellSet _winningMiddleCellSet = new(new List<BoardCells> { BoardCells.MiddleLeft, BoardCells.Middle, BoardCells.MiddleRigth });
+    private readonly WinningCellSet _winningDownCellSet = new(new List<BoardCells> { BoardCells.DownLeft, BoardCells.DownMiddle, BoardCells.DownRigth });
+    private readonly WinningCellSet _winningFirstColumnsCellSet = new(new List<BoardCells> { BoardCells.TopLeft, BoardCells.MiddleLeft, BoardCells.DownLeft });
+    private readonly WinningCellSet _winningMiddleColumnsCellSet = new(new List<BoardCells> { BoardCells.TopMiddle, BoardCells.Middle, BoardCells.DownMiddle });
+    private readonly WinningCellSet _winningLastColumnsCellSet = new(new List<BoardCells> { BoardCells.TopRigth, BoardCells.MiddleRigth, BoardCells.DownRigth });
+    private readonly WinningCellSet _winningADiagonalColumnsCellSet = new(new List<BoardCells> { BoardCells.TopLeft, BoardCells.Middle, BoardCells.DownRigth });
+    private readonly WinningCellSet _winningADiagonalColumnsCellSet2 = new(new List<BoardCells> { BoardCells.TopRigth, BoardCells.Middle, BoardCells.DownLeft });
+
+    private readonly WinningCellSet _playFinishWhenAllCellsAreTaken = new(new() { BoardCells.TopLeft, BoardCells.TopMiddle, BoardCells.TopRigth, BoardCells.MiddleLeft, BoardCells.Middle,BoardCells.MiddleRigth, BoardCells.DownLeft, BoardCells.DownMiddle, BoardCells.DownRigth });
+
+    public Board()
+    {
+        _winningCellSetList = new() {_winningTopCellSet,_winningMiddleCellSet,_winningDownCellSet, _winningFirstColumnsCellSet, _winningMiddleColumnsCellSet, _winningLastColumnsCellSet, _winningADiagonalColumnsCellSet, _winningADiagonalColumnsCellSet2 };
+    }
 
     public static void Main() { }
 
     public void Play(Player player, BoardCells boardCells)
     {
         if (AllCellsOfTheBoardCompleted()) throw new GameOver("Game over, the board it is completed.");
-            CheckPlayerTurn(player);
+        CheckPlayerTurn(player);
         CheckForValidPosition(boardCells, player);
     }
 
@@ -42,19 +59,7 @@ public class Board
 
     private void CheckIfAnyPlayerHasWon()
     {
-
-        var winningTopCellSet = new WinningCellSet(new() { BoardCells.TopLeft, BoardCells.TopMiddle, BoardCells.TopRigth });
-        var winningMiddleCellSet = new WinningCellSet(new() { BoardCells.MiddleLeft, BoardCells.Middle, BoardCells.MiddleRigth });
-        var winningDownCellSet = new WinningCellSet(new() { BoardCells.DownLeft, BoardCells.DownMiddle, BoardCells.DownRigth });
-        var winningFirstColumnsCellSet = new WinningCellSet(new() { BoardCells.TopLeft, BoardCells.MiddleLeft, BoardCells.DownLeft });
-        var winningMiddleColumnsCellSet = new WinningCellSet(new() { BoardCells.TopMiddle, BoardCells.Middle, BoardCells.DownMiddle });
-        var winningLastColumnsCellSet = new WinningCellSet(new() { BoardCells.TopRigth, BoardCells.MiddleRigth, BoardCells.DownRigth });
-        var winningADiagonalColumnsCellSet = new WinningCellSet(new() { BoardCells.TopLeft, BoardCells.Middle, BoardCells.DownRigth });
-        var winningADiagonalColumnsCellSet2 = new WinningCellSet(new() { BoardCells.TopRigth, BoardCells.Middle, BoardCells.DownLeft });
-
-        List<WinningCellSet> winningCellSetList = new() {winningTopCellSet,winningMiddleCellSet,winningDownCellSet, winningFirstColumnsCellSet, winningMiddleColumnsCellSet, winningLastColumnsCellSet, winningADiagonalColumnsCellSet, winningADiagonalColumnsCellSet2 };
-
-        foreach (var player in from winningCellSet in winningCellSetList from player in _winningPlayers where CheckIfAPlayerHasWonByCompletingAWinningCellSet(player, winningCellSet.WinningCells) select player)
+        foreach (var player in from winningCellSet in _winningCellSetList from player in _winningPlayers where CheckIfAPlayerHasWonByCompletingAWinningCellSet(player, winningCellSet.WinningCells) select player)
         {
             _winner = player;
             return;
@@ -73,7 +78,6 @@ public class Board
 
     public bool AllCellsOfTheBoardCompleted()
     {
-        var playFinishWhenAllCellsAreTaken = new WinningCellSet(new() { BoardCells.TopLeft, BoardCells.TopMiddle, BoardCells.TopRigth, BoardCells.MiddleLeft, BoardCells.Middle,BoardCells.MiddleRigth, BoardCells.DownLeft, BoardCells.DownMiddle, BoardCells.DownRigth });
-        return playFinishWhenAllCellsAreTaken.WinningCells.All(cell => _boardCells.ContainsKey(cell));
+        return _playFinishWhenAllCellsAreTaken.WinningCells.All(cell => _boardCells.ContainsKey(cell));
     }
 }
